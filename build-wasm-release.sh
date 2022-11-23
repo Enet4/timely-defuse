bf1579da0f99e884cd1978dd13a6fde287b97c76
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-11-23 22:18:34 +0000

    Change build-wasm-release.sh
    wasm-opt doesn't seem to work

--- build-wasm-release.sh
+++ build-wasm-release.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env sh
 set -eu
 cargo build --release --target wasm32-unknown-unknown
-# optimize with wasm-opt
-wasm-opt -Oz -o target/wasm32-unknown-unknown/release/timely-defuse.opt.wasm \
-    target/wasm32-unknown-unknown/release/timely-defuse.wasm
-# replace
-cp -f target/wasm32-unknown-unknown/release/timely-defuse.opt.wasm \
-    target/wasm32-unknown-unknown/release/timely-defuse.wasm \
 
 wasm-bindgen --out-name timely-defuse \
     --out-dir wasm/target \
