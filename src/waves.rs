b50b59ad0d0b766e72890e75a183956261b12a59
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-11-25 15:22:09 +0000

    Change end game victory condition
    Require 250 points for victory pose.

--- src/waves.rs
+++ src/waves.rs
@@ -222,10 +222,10 @@ fn spawn_game_over(
 ) {
     if let Ok((guy_entity, mut guy_state, mut guy_velocity)) = query_guy.get_single_mut() {
         match scores.score {
-            -999_999..=199 => {
+            -999_999..=249 => {
                 *guy_state = GuyState::Ouch;
             }
-            200..=999_999 => {
+            250..=999_999 => {
                 *guy_state = GuyState::Victorious;
             }
             _ => {
